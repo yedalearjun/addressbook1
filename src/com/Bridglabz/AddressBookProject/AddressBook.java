af4f8c23d678b2f6975dcabef6107526be51a73c
ARJUN YEDALE <arjunyedale8748@gmail.com>
2024-01-01 21:24:20 +0530

    welcome message added to master

---
+++ src/com/Bridglabz/AddressBookProject/AddressBook.java
@@ -0,0 +1,8 @@
+package com.Bridglabz.AddressBookProject;
+
+public class AddressBook {
+    public static void main(String[] args) {
+        System.out.println("welcome to addressbook");
+    }
+
+}
